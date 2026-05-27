a86ad3449cb39b7f00196f2bd35aee332f78c0aa
JaksonGardnir <140162243+JaksonGardnir@users.noreply.github.com>
2026-05-27 16:34:19 +1000

    Update formatting

--- prac_01/loops.py
+++ prac_01/loops.py
@@ -1,27 +1,27 @@
 for i in range(1, 21, 2):
     print(i, end=' ')  # The end=' ' adds a space
-print() # Goes to a new line.
+print()  # Goes to a new line.
 
-#a
+# a
 # Count in 10s from 0 to 100.
 for i in range(0, 101, 10):
     print(i, end=' ')
 print()
 
-#b
+# b
 # Count down from 20 to 1.
 for i in range(10, 0, -1):
     print(i, end=' ')
 print()
 
-#c
+# c
 # Print a number of stars.
 number_of_stars = int(input("Number of stars: "))
 for i in range(number_of_stars):
     print("*", end=' ')
 print()
 
-#d
+# d
 # Print lines of increasing stars.
 number_of_lines = int(input("Number of lines: "))
 for i in range(1, number_of_lines + 1):
