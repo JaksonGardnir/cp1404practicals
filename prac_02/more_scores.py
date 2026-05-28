4fa47b98faa3527ae7d6e5d6d95103936e9d6baf
JaksonGardnir <140162243+JaksonGardnir@users.noreply.github.com>
2026-05-28 15:35:05 +1000

    Write randomly generated scores to text file

--- prac_02/more_scores.py
+++ prac_02/more_scores.py
@@ -1,7 +1,30 @@
 """
 Program to assess randomly generated scores.
 """
+import random
 
+def main():
+    number_of_scores = float(input("Number of scores: "))
+    while number_of_scores < 1 or number_of_scores % 1 > 0:
+        print("Invalid number of scores (must be a positive integer).")
+        number_of_scores = float(input("Number of scores: "))
+    number_of_scores = int(number_of_scores)
+
+    # Create an empty .txt file.
+    with open("results.txt", "w") as f:
+        f.write("")
+
+    for i in range(number_of_scores):
+        random_score = random.randint(0,100)
+        score_status = assess_score(random_score)
+        score_message = f"{random_score} is {score_status}"
+        print(score_message)
+        # Write string into .txt file.
+        with open("results.txt", "a") as f:
+            f.write(score_message + "\n")
+
+
+# TODO: Import this function from score.py
 def assess_score(score: float) -> str:
     if score > 100:
         score_status = "Invalid score"
@@ -17,7 +40,7 @@ def assess_score(score: float) -> str:
 
 
 
-
+main()
 
 
 
