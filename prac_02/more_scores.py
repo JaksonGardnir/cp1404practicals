e42ff9e8dab9aa4943b929037e18f159a7d4311e
JaksonGardnir <140162243+JaksonGardnir@users.noreply.github.com>
2026-05-28 14:41:19 +1000

    Add score assessment function

--- prac_02/more_scores.py
+++ prac_02/more_scores.py
@@ -1,3 +1,30 @@
 """
 Program to assess randomly generated scores.
 """
+
+def assess_score(score: float) -> str:
+    if score > 100:
+        score_status = "Invalid score"
+    elif score >= 90:
+        score_status = "Excellent"
+    elif score >= 50:
+        score_status = "Passable"
+    elif score >= 0:
+        score_status = "Bad"
+    else:
+        score_status = "Invalid score"
+    return score_status
+
+
+
+
+
+
+
+
+
+
+
+
+
+
