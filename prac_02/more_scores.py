1b1f2692338732a7dc8c8f92e751d61e25f0f1e4
JaksonGardnir <140162243+JaksonGardnir@users.noreply.github.com>
2026-05-28 16:52:33 +1000

    Add functions to create an empty text file and append to a text file

--- prac_02/more_scores.py
+++ prac_02/more_scores.py
@@ -3,29 +3,48 @@ Program to assess randomly generated scores.
 """
 import random
 
+FILENAME = "results"
+LOWEST_SCORE = 0
+HIGHEST_SCORE = 100
+
+
 def main():
+    """Write randomly generated scores to .txt file."""
+    number_of_scores = get_number_of_scores()
+    create_empty_text_file(FILENAME)
+    for _ in range(number_of_scores):
+        random_score = random.randint(LOWEST_SCORE, HIGHEST_SCORE)
+        score_status = assess_score(random_score)
+        score_message = f"{random_score} is {score_status}\n"
+        append_to_text_file(FILENAME, score_message)
+        print(score_message)
+    print(f"An assessment of {number_of_scores} random scores has been printed to {FILENAME}.txt")
+
+
+def create_empty_text_file(filename: str):
+    """Create an empty file filename.txt"""
+    with open(filename + ".txt", "w", encoding="utf-8") as f:
+        f.write("")
+
+
+def append_to_text_file(filename: str, message: str):
+    """Append a string to filename.txt."""
+    with open(filename + ".txt", "a", encoding="utf-8") as f:
+        f.write(message)
+
+
+def get_number_of_scores() -> int:
+    """Get a positive integer number of scores."""
     number_of_scores = float(input("Number of scores: "))
     while number_of_scores < 1 or number_of_scores % 1 > 0:
         print("Invalid number of scores (must be a positive integer).")
         number_of_scores = float(input("Number of scores: "))
-    number_of_scores = int(number_of_scores)
-
-    # Create an empty .txt file.
-    with open("results.txt", "w") as f:
-        f.write("")
-
-    for i in range(number_of_scores):
-        random_score = random.randint(0,100)
-        score_status = assess_score(random_score)
-        score_message = f"{random_score} is {score_status}"
-        print(score_message)
-        # Write string into .txt file.
-        with open("results.txt", "a") as f:
-            f.write(score_message + "\n")
+    return int(number_of_scores)
 
 
 # TODO: Import this function from score.py
 def assess_score(score: float) -> str:
+    """Return a score status based on input score."""
     if score > 100:
         score_status = "Invalid score"
     elif score >= 90:
@@ -39,15 +58,4 @@ def assess_score(score: float) -> str:
     return score_status
 
 
-
 main()
-
-
-
-
-
-
-
-
-
-
