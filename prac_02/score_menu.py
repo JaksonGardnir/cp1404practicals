7fd21aec2156c123eb2c9deab4b4a399f2251d9f
JaksonGardnir <140162243+JaksonGardnir@users.noreply.github.com>
2026-05-28 13:54:22 +1000

    Add functionality to print result

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,7 +1,6 @@
 """
 Program that utilises a menu to perform actions related to the users score.
 """
-import random
 
 MENU = """
 (G)et a valid score.
@@ -20,7 +19,8 @@ def main():
         if choice == "G":
             valid_score = get_valid_score()
         elif choice == "P":
-            pass
+            score_status = assess_score(valid_score)
+            print(f"User score {valid_score} is {score_status}.")
         elif choice == "S":
             pass
         else:
@@ -37,6 +37,20 @@ def get_valid_score() -> float:
         print("Invalid score (must be between 0 and 100 inclusive).")
         valid_score = float(input("Input score: "))
     return valid_score
+
+
+def assess_score(score: float) -> str:
+    if score > 100:
+        score_status = "Invalid score"
+    elif score >= 90:
+        score_status = "Excellent"
+    elif score >= 50:
+        score_status = "Passable"
+    elif score >= 0:
+        score_status = "Bad"
+    else:
+        score_status = "Invalid score"
+    return score_status
 
 
 def print_greeting(name):
